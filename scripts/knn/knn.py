8c18ad405b74a73ad9d5fab1ae74a03751c58abe
Aaron <amwayne90@gmail.com>
2017-02-20 00:09:13 -0800

    avg images and results changes

--- scripts/knn/knn.py
+++ scripts/knn/knn.py
@@ -1,5 +1,6 @@
 '''
 Using the CIFAR-10 Data Set to write a k-Nearest Neighbors algorithm
+32x32 - red, green, blue
 '''
 import sys
 sys.path.insert(0, '../../util/')
@@ -7,10 +8,22 @@ import numpy as np
 import scipy.stats as stats
 import matplotlib as plt
 import processing as pr
+from PIL import Image
+from datetime import datetime
 
-def knn(X_train,Y_train, X_test, k):
-    results = np.empty(100)
-    for i in range(100): #range(X_test.shape[0]):
+NUM_TO_COMPLETE = 1000 # 1 to 10,000 for one batch, 50,000 for all.
+NUM_COLORS = 3
+IMG_WIDTH = 32
+NUM_BATCHES = 1 # 1 to 5
+FOLDER_PRE = '../../data'
+DATA_SOURCE = '/cifar-10-batches-py/'
+CLASS_NAMES = pr.unpickle(FOLDER_PRE + DATA_SOURCE + 'batches.meta')['label_names']
+
+
+def get_knn_classes(X_train,Y_train, X_test, k):
+    results = np.empty(NUM_TO_COMPLETE)
+    for i in range(NUM_TO_COMPLETE): #range(X_test.shape[0]):
+    	print "Image:", i
         total_diffs = np.dot(abs(np.subtract(X_train, X_test[i])), 
         	np.ones(X_train.shape[1]))
         # Sort total_diffs and get the indices of the k smallest values
@@ -19,21 +32,76 @@ def knn(X_train,Y_train, X_test, k):
         results[i] = np.argmax(np.bincount(Y_train[indices]))
     return results
 
-if __name__== "__main__":
-	for k in range(3,11):
-		print
-		print "Working on:", k
-		traindata = pr.unpickle('../../data/cifar-10-batches-py/data_batch_1')
-		testdata = pr.unpickle('../../data/cifar-10-batches-py/test_batch')
-		
-		X_train = np.array(traindata['data']).astype(int)
-		Y_train = np.array(traindata['labels']).astype(int)
-		X_test = np.array(testdata['data']).astype(int)
-		Y_test = np.array(testdata['labels']).astype(int)
+def knn(X_train, Y_train, X_test, Y_test, k):
+	results = get_knn_classes(X_train,Y_train, X_test, k).astype(int)
+	np.savetxt("./results/" + str(datetime.now())[:-7] + " k_" + str(k) + " knn array.csv", results, delimiter = ',', fmt = '%1d')
+	test_diff = np.subtract(results,Y_test[:NUM_TO_COMPLETE])
+	num_incorrect = np.count_nonzero(test_diff)
+	test_diff[test_diff > 0] = 1
+	test_diff[test_diff < 0] = 1
+	# correct classifications in this array are 0. Add 1 to all class labels (now 1 to 10) to differentiate them from correct answers (0)
+	incorrect_classifications = np.bincount(test_diff * (Y_test[:NUM_TO_COMPLETE] + 1))[1:]
+	
+	overall_perc_correct = 1 - float(num_incorrect) / len(results)
+	return (1 - incorrect_classifications.astype(float) / np.bincount(Y_test[:NUM_TO_COMPLETE])), overall_perc_correct
 
-		results = knn(X_train,Y_train, X_test, k).astype(int)
-		np.savetxt("./results/knn_array.csv", results, delimiter = ',', fmt = '%1d')
-		test_diff = np.subtract(results,Y_test[:100])
-		num_incorrect = np.count_nonzero(test_diff)
-		perc_correct = 1 - float(num_incorrect) / len(results)
-		print "Percentage correct: ", perc_correct
+def produce_average_pixel_images(X_train, Y_train, CLASS_NAMES):
+	for i in np.unique(Y_train):
+		print "Producing image: ", i
+		indices = np.where(Y_train == i)[0]
+		l = len(indices)
+		# Take the average of each pixel value across all images of the same class
+		avg = (np.dot(X_train[indices].T, np.ones(l)) / l).astype(int)
+		pixels = np.array(np.split(avg, NUM_COLORS)).T
+		pic = np.array(np.split(pixels, IMG_WIDTH))
+		im = Image.fromarray(np.uint8(pic))
+
+		im.save("./avg_imgs/" + str(i) + '_' + CLASS_NAMES[i] + ".png")
+
+
+def concat_datasets(filename, X_data=np.array([]), Y_data=np.array([])):
+	batch = pr.unpickle(filename)
+	if len(X_data) == 0:
+		return np.array(batch['data']).astype(int), np.array(batch['labels']).astype(int)
+	else:
+		return np.concatenate((X_data, np.array(batch['data']).astype(int)), axis = 0), np.concatenate((Y_data, np.array(batch['labels']).astype(int)), axis = 0)
+
+def print_results_file(results, pc, k):
+	dt = str(datetime.now())
+	filename = './results/' + dt[:-7] + ' k_' + str(k) + '.txt'
+	outfile = open(filename, 'w')
+	outfile.write('KNN Results\n')
+	outfile.write('\n')
+	outfile.write('Data source: ' + DATA_SOURCE + '\n')
+	outfile.write('k: ' + str(k) + '\n')
+	outfile.write('datetime:  ' + dt + '\n')
+	outfile.write('Number of images included: ' + str(NUM_TO_COMPLETE) + '\n')
+	outfile.write('\n')
+	outfile.write('\n')
+	outfile.write('Overall accuracy: ' + str(pc) + '\n')
+	outfile.write('\n')
+	outfile.write('Accuracy by class:\n')
+	for i in range(len(CLASS_NAMES)):
+		outfile.write(CLASS_NAMES[i] + ' : ' + str(results[i]) + '\n')
+	outfile.close()
+
+if __name__== "__main__":
+	
+	#fake_results = [0,1,2,3,4,5,6,7,8,9]
+	#print_results_file(fake_results, 0.5, 10)
+
+	X_train = np.array([])
+	Y_train = np.array([])
+	for i in range(1, NUM_BATCHES+1):
+		X_train, Y_train = concat_datasets(FOLDER_PRE + DATA_SOURCE + 'data_batch_' + str(i), X_train, Y_train)
+	X_test, Y_test = concat_datasets(FOLDER_PRE + DATA_SOURCE + 'test_batch')
+	
+	#produce_average_pixel_images(X_train, Y_train, CLASS_NAMES)
+	for k in range(10,11):
+		print "Working on:", k
+		results, pc = knn(X_train, Y_train, X_test, Y_test, k)
+		print_results_file(results, pc, k)
+		
+
+		
+
