5623bdbe485e23862558ba2b332ce1b9a1c8a2cb
Aaron <amwayne90@gmail.com>
2017-02-22 23:04:41 -0800

    fix save attributes for svm

--- scripts/svm/svm.py
+++ scripts/svm/svm.py
@@ -18,6 +18,9 @@ NUM_COLORS = 3
 IMG_WIDTH = 32
 NUM_BATCHES = 1 # 1 to 5
 
+loss_options = ['hinge', 'square_hinge']
+penalty_options = ['l1', 'l2']
+
 if __name__== "__main__":
 	i=1
 	filename = FOLDER_PRE + DATA_SOURCE + 'data_batch_' + str(i)
@@ -36,7 +39,6 @@ if __name__== "__main__":
 	pred = lin_clf.predict(X_test)
 
 	diff = (pred - y_test)
-
 	incorrect = np.divide(diff, diff) # 0 is correct, 1 is incorrect
 
 	# +1 to the classes on y_test (now 1 to 10) to differentiate it from 0 (a correct prediction)
@@ -44,6 +46,7 @@ if __name__== "__main__":
 
 	pc_by_class = 1 - incorrect_by_class.astype(float) / np.bincount(y_test).astype(float)
 
+	np.set_printoptions(threshold=np.nan)
 	pr.print_results_file(dict(
 			pc_overall=(1.0 - sum(incorrect).astype(float) / NUM_TO_COMPLETE),
 			pc_by_class=pc_by_class,
@@ -53,6 +56,8 @@ if __name__== "__main__":
 			method='svm linear',
 			other=dict(
 				params=str(lin_clf),
+				coefficients=lin_clf.coef_,
+				intercepts=lin_clf.intercept_,
 				),
 		)
 	)
