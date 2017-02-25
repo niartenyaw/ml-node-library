0d6fc11e8f0da1ddbb5e5219fc3da5763c4931ae
Aaron <amwayne90@gmail.com>
2017-02-25 15:40:25 -0800

    small changes on mac

--- scripts/svm/svm.py
+++ scripts/svm/svm.py
@@ -13,52 +13,59 @@ FOLDER_PRE = '../../data'
 DATA_SOURCE = '/cifar-10-batches-py/'
 CLASS_NAMES = pr.unpickle(FOLDER_PRE + DATA_SOURCE + 'batches.meta')['label_names']
 
-NUM_TO_COMPLETE = 100
+NUM_TO_COMPLETE = 10000
 NUM_COLORS = 3
 IMG_WIDTH = 32
 NUM_BATCHES = 1 # 1 to 5
 
-loss_options = ['hinge', 'square_hinge']
+loss_options = ['hinge', 'squared_hinge']
 penalty_options = ['l1', 'l2']
 
 if __name__== "__main__":
-	i=1
-	filename = FOLDER_PRE + DATA_SOURCE + 'data_batch_' + str(i)
-	b_train = pr.unpickle(filename)
-	X_train = np.array(b_train['data'])[:NUM_TO_COMPLETE].astype(int)
-	y_train = np.array(b_train['labels'])[:NUM_TO_COMPLETE].astype(int)
+	for loss in loss_options:
+		for penalty in penalty_options:
+			print "Working on:", loss, penalty
+			i=1
+			filename = FOLDER_PRE + DATA_SOURCE + 'data_batch_' + str(i)
+			b_train = pr.unpickle(filename)
+			X_train = np.array(b_train['data'])[:NUM_TO_COMPLETE].astype(int)
+			y_train = np.array(b_train['labels'])[:NUM_TO_COMPLETE].astype(int)
 
-	filename = FOLDER_PRE + DATA_SOURCE + 'data_batch_' + str(i+1)
-	b_test = pr.unpickle(filename)
-	X_test = np.array(b_test['data'])[:NUM_TO_COMPLETE].astype(int)
-	y_test = np.array(b_test['labels'])[:NUM_TO_COMPLETE].astype(int)
+			filename = FOLDER_PRE + DATA_SOURCE + 'data_batch_' + str(i+1)
+			b_test = pr.unpickle(filename)
+			X_test = np.array(b_test['data'])[:NUM_TO_COMPLETE].astype(int)
+			y_test = np.array(b_test['labels'])[:NUM_TO_COMPLETE].astype(int)
+			try:
+				lin_clf = svm.LinearSVC(penalty=penalty, loss=loss)
+				lin_clf.fit(X_train, y_train)
 
-	lin_clf = svm.LinearSVC()
-	lin_clf.fit(X_train, y_train)
+				pred = lin_clf.predict(X_test)
 
-	pred = lin_clf.predict(X_test)
+				diff = (pred - y_test)
+				incorrect = np.divide(diff, diff) # 0 is correct, 1 is incorrect
 
-	diff = (pred - y_test)
-	incorrect = np.divide(diff, diff) # 0 is correct, 1 is incorrect
+				# +1 to the classes on y_test (now 1 to 10) to differentiate it from 0 (a correct prediction)
+				incorrect_by_class = np.bincount(incorrect * (y_test+1))[1:]
 
-	# +1 to the classes on y_test (now 1 to 10) to differentiate it from 0 (a correct prediction)
-	incorrect_by_class = np.bincount(incorrect * (y_test+1))[1:]
+				pc_by_class = 1 - incorrect_by_class.astype(float) / np.bincount(y_test).astype(float)
 
-	pc_by_class = 1 - incorrect_by_class.astype(float) / np.bincount(y_test).astype(float)
-
-	np.set_printoptions(threshold=np.nan)
-	pr.print_results_file(dict(
-			pc_overall=(1.0 - sum(incorrect).astype(float) / NUM_TO_COMPLETE),
-			pc_by_class=pc_by_class,
-			class_names=CLASS_NAMES,
-			data_source=DATA_SOURCE,
-			num_images=NUM_TO_COMPLETE,
-			method='svm linear',
-			other=dict(
-				params=str(lin_clf),
-				coefficients=lin_clf.coef_,
-				intercepts=lin_clf.intercept_,
-				),
-		)
-	)
+				np.set_printoptions(threshold=np.nan)
+				pr.print_results_file(dict(
+						pc_overall=(1.0 - sum(incorrect).astype(float) / NUM_TO_COMPLETE),
+						pc_by_class=pc_by_class,
+						class_names=CLASS_NAMES,
+						data_source=DATA_SOURCE,
+						num_images=NUM_TO_COMPLETE,
+						method='svm linear',
+						other=dict(
+							penalty=penalty,
+							loss=loss,
+							params=str(lin_clf),
+							coefficients=lin_clf.coef_,
+							intercepts=lin_clf.intercept_,
+							),
+					)
+				)
+			except:
+				print loss, penalty, "not working.."
 
